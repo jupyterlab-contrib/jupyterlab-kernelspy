6cd8d50dbaab2ff695f57df399dcdbdacfa1a0a0
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-08-04 15:31:34 +0000

    lint

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,8 @@ import {
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-kernelspy:plugin',
-  description: 'A Jupyter Lab extension for inspecting messages to/from a kernel',
+  description:
+    'A Jupyter Lab extension for inspecting messages to/from a kernel',
   autoStart: true,
   activate: (app: JupyterFrontEnd) => {
     console.log('JupyterLab extension jupyterlab-kernelspy is activated!');
