577a67845f8204b5605bf9ed14f54021f8f9d206
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-08-04 14:46:22 +0000

    Update to JupyterLab 4

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+/**
+ * Initialization data for the jupyterlab-kernelspy extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: 'jupyterlab-kernelspy:plugin',
+  description: 'A Jupyter Lab extension for inspecting messages to/from a kernel',
+  autoStart: true,
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension jupyterlab-kernelspy is activated!');
+  }
+};
+
+export default plugin;
